baecf6f0094f1ba0edfdbfda367b558d36816c11
Studio Loading <studioloadingproductions@gmail.com>
2023-01-02 18:22:50 +0100

    Rightleft indicator on Blocked Motherpl

--- src/SpriteRightleft.c
+++ src/SpriteRightleft.c
@@ -1,6 +1,8 @@
 #include "Banks/SetAutoBank.h"
 
 #include "main.h"
+
+#include "Keys.h"
 #include "ZGBMain.h"
 #include "Scroll.h"
 #include "Sprite.h"
@@ -9,14 +11,19 @@
 
 const UINT8 rightleft_anim[] = {2, 0, 1};
 
+extern Sprite* s_motherpl;
+
 void START(){
     THIS->lim_x = 255u;
     THIS->lim_y = 255u;
-    SetSpriteAnim(THIS, rightleft_anim, 8u);
+    SetSpriteAnim(THIS, rightleft_anim, 20u);
 }
 
-void UPDATE(){}
+void UPDATE(){
+    //THIS->x = (UINT16)s_motherpl->x;
+    //THIS->y = (UINT16)s_motherpl->y - 24u;
+}
 
 void DESTROY(){
-     THIS->lim_x = 255u;
+    //THIS->lim_x = 255u;
 }
